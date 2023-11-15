b013b5a03f4d1b648d3d85c166e420894b36d044
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2023-11-14 20:01:59 -0500

    Add files via upload

--- starburst.c
+++ starburst.c
@@ -174,7 +174,7 @@ gegl_op_class_init (GeglOpClass *klass)
   operation_class->attach = attach;
 
   gegl_operation_class_set_keys (operation_class,
-    "name",        "gegl:starburst",
+    "name",        "lb:starburst",
     "title",       _("Starburst"),
     "categories",  "Artistic",
     "reference-hash", "45ed16360128agbr25493xc254001b2ac",
